37197b435470d11b5acab694d66af6e6a00f8939
Michael Matheus <michaelmatheus2001@gmail.com>
2022-01-27 20:59:45 -0300

    feat: aula #4 - array (módulo 3 - conceitos básicos)

---
+++ Módulo 3 - Conceitos Básicos/#4 - Array/script.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ * Os arrays servem básicamente para guardar diversas informações dentro de uma variável economizando espaço na memória do computador
+ * 
+ */
+
+
+// exemplo:
+let carros = ['Palio', 'Uno', 'Corolla', 'Ferrari'];
+
+/** chamada do array:
+ * 
+ * console.log(carros[0]); 
+ * 
+*/
+
+
+/** forma antiga de chamada de arrays:
+ * 
+ * let carros = new Array('Palio', 'Uno', 'Corolla', 'Ferrari');
+ * 
+*/
+
+
+// array multidimensional (camadas):
+let ingredientes = [
+    ['uva', 'pêra', 'maça'],
+    ['arroz', 'macarrão']
+];
+
+/** como chamar o indice pêra
+ * 
+ * console.log(ingredientes[0][1]); 
+ * 
+*/
