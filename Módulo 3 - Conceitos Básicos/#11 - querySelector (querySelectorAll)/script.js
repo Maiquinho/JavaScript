850a0e85708b4b26262d382a418dab7fb4974032
Michael Matheus <michaelmatheus2001@gmail.com>
2022-01-30 14:43:55 -0300

    feat: aula #11 - querySelector (querySelectorAll) (módulo 3 - conceitos básicos)

---
+++ Módulo 3 - Conceitos Básicos/#11 - querySelector (querySelectorAll)/script.js
@@ -0,0 +1,27 @@
+/**
+ * O querySelector é utilizado para retornar o primeiro elemento, classe, id, ou a junção de um elemento + class ou elemento + id.
+ * 
+ * 
+ * */ 
+
+// exemplos: 
+
+document.querySelector('.lista'); // retorna o primeiro elemento com a classe lista
+
+document.querySelector('ol.lista'); // retorna o primeiro elemento ol com a classe lista
+
+document.querySelector('#titulo'); // retorna o primeiro elemento com o id titulo
+
+
+
+/**
+ * O querySelectorAll é utilizado para retornar todos os elemento, classe, id, ou a junção de um elemento + class ou elemento + id em uma NodeList.
+ * 
+ * 
+ * */ 
+
+// exemplos: 
+
+document.querySelectorAll('.lista'); // retorna todos os elementos com a classe lista
+
+document.querySelectorAll('li'); // retorna todos os elementos div
