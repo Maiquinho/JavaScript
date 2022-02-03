597fa7914a3206ea70ae7c8cf87ed8fccb812ed7
Michael Matheus <michaelmatheus2001@gmail.com>
2022-02-02 23:45:45 -0300

    feat: aula #4 - métodos de numbers  (módulo 4 - manipulações, arrow, ajax, etc)

---
+++ Módulo 4 - Manipulações, Arrow, Ajax, etc/#4 - Métodos de Numbers/script.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * O Método .toString() transforma um valor numérico em uma string.
+ * 
+ * 
+ * O Método toFixed() formata um número de acordo com o valor de casas decimais especificado pelo parâmetro.
+ * 
+ * O parseInt(x) transforma o valor especificado no parâmetro em um inteiro.
+ * 
+ * 
+ * O parseFloat(x) transforma o valor especificado no parâmetro em um número de ponto flutuante.
+ * 
+ * 
+ * 
+*/
+
+// exemplos: 
+
+let a = 10;
+
+let b = 10.6546572645;
+
+let c = '25';
+
+let d = '45.2387';
+
+// a.toString(); // retorna: '10'
+
+// b.toFixed(2); // retorna: 10.65
+
+// parseInt(c);  // retorna: 25
+
+// parseFloat(d);  // retorna: 45.2387
