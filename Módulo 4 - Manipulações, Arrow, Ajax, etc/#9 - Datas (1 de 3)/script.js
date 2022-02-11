be147f05c69994b964befcb5c674da542777e91f
Michael Matheus <michaelmatheus2001@gmail.com>
2022-02-10 21:28:28 -0300

    feat: aula #9 - datas (1/3) (módulo 4 - manipulações, arrow, ajax, etc)

---
+++ Módulo 4 - Manipulações, Arrow, Ajax, etc/#9 - Datas (1 de 3)/script.js
@@ -0,0 +1,22 @@
+/**
+ *
+ * Date cria uma instância JavaScript de Date que representa um único momento no tempo. Objetos Date são baseados no valor de tempo que é o número de milisegundos desde 1º de Janeiro de 1970 (UTC).
+ * 
+ *   
+ * O método toDateString() retorna a porção da data de um objeto Date em um formato legível para humanos em inglês dos Estados Unidos.
+ * 
+ * 
+ * O método toString() retorna uma cadeia de caracteres (string) representando o objeto Date especificado.
+ * 
+ * 
+ * O método toUTCString() converte uma data para uma cadeia de caracteres (string), usando o fuso horário UTC.
+ * 
+ * 
+ * 
+*/
+
+
+let date = new Date('2001-04-23 02:29:00');
+
+
+console.log(date)
