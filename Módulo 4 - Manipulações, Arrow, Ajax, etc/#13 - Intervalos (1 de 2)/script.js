579d6875a56e72e64d9a79aa4bfc27ed869eaf5b
Michael Matheus <michaelmatheus2001@gmail.com>
2022-02-18 19:25:00 -0300

    feat(description): lesson #13 - intervals (1/2)  (module 4 - manipulations, arrow, ajax...)

--- Módulo 4 - Manipulações, Arrow, Ajax, etc/#13 - Intervalos (1 de 2)/script.js
+++ Módulo 4 - Manipulações, Arrow, Ajax, etc/#13 - Intervalos (1 de 2)/script.js
@@ -1,6 +1,7 @@
 /**
  * 
- * 
+ * O método setInterval() oferecido das interfaces Window e Worker, repertem chamadas de funções ou executam trechos de código, com um tempo de espera fixo entre cada chamada. Isso retorna um ID único para o intervalo, podendo remove-los mais tarde apenas o chamando clearInterval().
+ * Este método é definido pelo mixin WindowOrWorkerGlobalScope.
  * 
 */
 
