3d6fb0e1cf7efd1a376f610ce5fb40bb8ff837d0
Michael Matheus <michaelmatheus2001@gmail.com>
2022-05-21 12:56:10 -0300

    feat: lesson #1 - what is a request? (module 5 - requests and async)

---
+++ Módulo 5 - Requisições e assíncronismo/#1 - O que são requisições/script.js
@@ -0,0 +1,6 @@
+/**
+ * 
+ * Requisições HTTP são mensagens enviadas pelo cliente para iniciar uma ação no servidor. Suas linhas iniciais contêm três elementos: Um método HTTP, um verbo (como GET, PUT ou POST) ou um nome (como HEAD ou OPTIONS), que descrevem a ação a ser executada.
+ * 
+ *
+*/
