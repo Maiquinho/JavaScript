d742d352cbb808105e870fc1df6cd086a1ebc1bb
Michael Matheus <michaelmatheus2001@gmail.com>
2022-05-21 17:05:13 -0300

    feat: lesson #2 - what is JSON? (module 5 - requests and async)

---
+++ Módulo 5 - Requisições e assíncronismo/#2 - O que é um JSON/script.js
@@ -0,0 +1,5 @@
+/**
+ * 
+ * O Objeto JSON contém métodos para parsing JavaScript Object Notation (JSON) e conversão de valores para JSON. Ele não pode ser chamado ou construído e, além de suas propriedades de dois métodos, ele não possui uma funcionalidade interesante.
+ *
+*/
